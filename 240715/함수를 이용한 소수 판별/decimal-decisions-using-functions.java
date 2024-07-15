c80d1fd623d523f7b3e60632306de45cd5eb174f
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2024-07-15 15:43:11 +0900

    Title: 함수를 이용한 소수 판별, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240715/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 240715/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -9,6 +9,8 @@ public class Main {
         int sum = 0;
 
         for(int i = a; i <= b; i++) {
+            if(i == 1) continue;
+            
             if(isPrime(i)) {
                 sum += i;
             }
