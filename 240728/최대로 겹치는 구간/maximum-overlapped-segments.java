3a366f0e1c861420e786a9bf4c711c39e62d93ab
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2024-07-28 23:24:47 +0900

    Title: 최대로 겹치는 구간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240728/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[201];
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            for(int j = a; j < b; j++) {
+                arr[j]++;
+            }
+        }
+
+        int max = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            if(max < arr[i]) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println(max);
+        
+    }
+}
