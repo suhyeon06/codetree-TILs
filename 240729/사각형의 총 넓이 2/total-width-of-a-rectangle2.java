a6878a8ebe3006c96da1487cc581b1e2581a81c2
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2024-07-29 17:16:47 +0900

    Title: 사각형의 총 넓이 2, Time: 100ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240729/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static final int OFFSET = 100;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[201][201];
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            x1 += OFFSET;
+            y1 += OFFSET;
+            x2 += OFFSET;
+            y2 += OFFSET;
+
+            for(int j = x1; j < x2; j++) {
+                for(int k = y1; k < y2; k++) {
+                    arr[j][k] = 1;
+                }
+            }
+        }
+
+        int cnt = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length; j++) {
+                if(arr[i][j] == 1) {
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+        
+    }
+}
