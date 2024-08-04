4c7f7bdac8956623d3e2cc4f3069710ce55111eb
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2024-08-04 23:38:03 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 123ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240804/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static final int OFFSET = 100;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int[][] arr = new int[2001][2001];
+
+        for(int i = 0; i < 2; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            x1 += OFFSET;
+            y1 += OFFSET;
+            x2 += OFFSET;
+            y2 += OFFSET;
+
+            for(int j = x1; j < x2; j++) {
+                for(int k = y1; k < y2; k++) {
+                    if(i == 0) {
+                        arr[j][k] = 1;
+                    } else {
+                        arr[j][k] = 0;
+                    }
+                }
+            }
+        }
+
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length; j++) {
+                if(arr[i][j] == 1) {
+                    if (i < minX) minX = i;
+                    if (i > maxX) maxX = i;
+                    if (j < minY) minY = j;
+                    if (j > maxY) maxY = j;
+                }
+            }
+        }
+
+        int width = maxX - minX + 1;
+        int height = maxY - minY + 1;
+
+        System.out.println(width * height);
+    }
+}
