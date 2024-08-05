ef3c6fd0c67fa6a114773052ae74d1074c283a3a
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2024-08-05 13:46:09 +0900

    Title: 오름 내림차순 정렬, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240805/오름 내림차순 정렬/inc-dec-sorting.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
