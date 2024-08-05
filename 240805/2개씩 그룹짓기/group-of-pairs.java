2f1e0d0cdd76a6f802cfbe360b5beb080aa10a32
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2024-08-05 14:38:58 +0900

    Title: 2개씩 그룹짓기, Time: 117ms, Memory: 9MB, Status: Passed - CodeTree

--- 240805/2개씩 그룹짓기/group-of-pairs.java
+++ 240805/2개씩 그룹짓기/group-of-pairs.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        int[] arr = new int[n*2];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n*2; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
@@ -16,8 +16,8 @@ public class Main {
 
         int max = 0;
 
-        for(int i = 0; i < n/2; i++) {
-            max = Math.max(max, arr[i]+arr[n-1-i]);
+        for(int i = 0; i < n; i++) {
+            max = Math.max(max, arr[i]+arr[n*2-1-i]);
         }
 
         System.out.println(max);
